f3882d9b560d7099be718eecab1fbbe1ee78baea
yoba <yobapochta@gmail.com>
2025-02-28 08:26:29 -0500

    style improvements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,27 @@ function Card({ card, faceDown, small, onClick, disabled }) {
     <div 
       className={`card ${faceDown ? 'face-down' : ''} ${small ? 'small' : ''} ${disabled ? 'disabled' : ''}`}
       onClick={disabled ? undefined : onClick}
-      style={!faceDown ? { borderTop: `3px solid ${card.color}` } : {}}
+      style={!faceDown ? { 
+        borderTop: `3px solid ${card.color}`,
+      } : {}}
     >
       {!faceDown && (
         <div className="card-content">
-          <span className="card-word">{card.word}</span>
-          <span className="card-hint">{card.hint}</span>
-          <span className="card-category" style={{ color: card.color }}>
-            {card.category}
-          </span>
+          <div className="card-main-content">
+            <span className="card-word" style={{ whiteSpace: 'pre-wrap' }}>{card.word}</span>
+          </div>
+          <div className="card-bottom">
+            <span className="card-hint" style={{ whiteSpace: 'pre-wrap' }}>{card.hint}</span>
+            <span 
+              className="card-category" 
+              style={{ 
+                color: card.color,
+                whiteSpace: 'pre-wrap'
+              }}
+            >
+              {card.category}
+            </span>
+          </div>
         </div>
       )}
     </div>
